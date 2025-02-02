08a0db8d587b00b34e87968b9d9254a245d8b112
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-02-02 11:04:46 +0700

    feat: add server cheking endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,20 @@ async function init(){
         console.log("database status", result)
 
         const app = express();
+
         app.use(bodyParser.json());
+
         const PORT = 3000;
+
+        app.get("/", (req, res) => {
+            res.status(200).json({
+                message: "Server is running",
+                data: null
+            })
+        })
+
         app.use('/api', router);
+
         app.listen(PORT, () => {
             console.log(`Server is running on port http://localhost:${PORT}`);
         });
